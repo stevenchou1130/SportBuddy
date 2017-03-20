50f43f77325fa5b52088feed06d69e31567d5180
Steven Chou <stevennote1130@gmail.com>
2017-03-20 16:09:40 +0800

    project setting

--- SportBuddy/LoginViewController.swift
+++ SportBuddy/LoginViewController.swift
@@ -20,7 +20,6 @@ class LoginViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
 
     /*
     // MARK: - Navigation
