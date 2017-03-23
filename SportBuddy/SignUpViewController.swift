9cf64d4e0c29089d04942655e24134459941915e
Steven Chou <stevennote1130@gmail.com>
2017-03-23 11:53:40 +0800

    Add gender radio button to sign up page

--- SportBuddy/SignUpViewController.swift
+++ SportBuddy/SignUpViewController.swift
@@ -14,9 +14,19 @@ class SignUpViewController: BaseViewController {
     @IBOutlet weak var accountTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var genderLabel: UILabel!
+
+    @IBOutlet weak var maleButton: UIButton!
+    @IBOutlet weak var femaleButton: UIButton!
+
+    var maleRadioButton = LTHRadioButton()
+    var femaleRadioButton = LTHRadioButton()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        maleRadioButton = LTHRadioButton(selectedColor: .blue)
+        femaleRadioButton = LTHRadioButton(selectedColor: .blue)
 
         setView()
     }
@@ -24,6 +34,36 @@ class SignUpViewController: BaseViewController {
     func setView() {
         userImage.layer.cornerRadius = userImage.bounds.size.width / 2.0
         userImage.layer.borderWidth = 1.0
+
+        // Male Radio Button
+        view.addSubview(maleRadioButton)
+        maleRadioButton.translatesAutoresizingMaskIntoConstraints = false
+
+        NSLayoutConstraint.activate([
+            maleRadioButton.centerYAnchor.constraint(equalTo: maleButton.centerYAnchor),
+            maleRadioButton.trailingAnchor.constraint(equalTo: maleButton.leadingAnchor, constant: -10),
+            maleRadioButton.heightAnchor.constraint(equalToConstant: maleRadioButton.frame.height),
+            maleRadioButton.widthAnchor.constraint(equalToConstant: maleRadioButton.frame.width)])
+
+        // Female Radio Button
+        view.addSubview(femaleRadioButton)
+        femaleRadioButton.translatesAutoresizingMaskIntoConstraints = false
+
+        NSLayoutConstraint.activate([
+            femaleRadioButton.centerYAnchor.constraint(equalTo: femaleButton.centerYAnchor),
+            femaleRadioButton.trailingAnchor.constraint(equalTo: femaleButton.leadingAnchor, constant: -10),
+            femaleRadioButton.heightAnchor.constraint(equalToConstant: femaleRadioButton.frame.height),
+            femaleRadioButton.widthAnchor.constraint(equalToConstant: femaleRadioButton.frame.width)])
+    }
+
+    @IBAction func cleckMaleButton(_ sender: Any) {
+        maleRadioButton.select()
+        femaleRadioButton.deselect(animated: false)
+    }
+
+    @IBAction func cleckFemaleButton(_ sender: Any) {
+        femaleRadioButton.select()
+        maleRadioButton.deselect(animated: false)
     }
 
     @IBAction func signUp(_ sender: Any) {
