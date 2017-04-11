0fcfa9be79a2448f8c4550e8c95964c25004ca79
Steven Chou <stevennote1130@gmail.com>
2017-04-11 17:48:57 +0800

    Create game UI - 70%

---
+++ SportBuddy/GameTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  GameTableViewCell.swift
+//  SportBuddy
+//
+//  Created by steven.chou on 2017/4/11.
+//  Copyright © 2017年 stevenchou. All rights reserved.
+//
+
+import UIKit
+
+class GameTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- SportBuddy/NewBasketballGameViewController.swift
+++ SportBuddy/NewBasketballGameViewController.swift
@@ -7,12 +7,64 @@
 //
 
 import UIKit
+import NVActivityIndicatorView
 
-class NewBasketballGameViewController: UIViewController {
+class NewBasketballGameViewController: BaseViewController {
+
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var timeTextField: UITextField!
+    @IBOutlet weak var courtTextField: UITextField!
+    @IBOutlet weak var levelTextField: UITextField!
+
+    var basketballCourts: [BasketballCourt] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        print("== NewBasketballGameViewController ==")
+        setView()
     }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+        getCourts()
+    }
+
+    func setView() {
+
+        self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for: UIBarMetrics.default)
+        self.navigationController?.navigationBar.shadowImage = UIImage()
+        self.navigationController?.navigationBar.isTranslucent = true
+        self.navigationController?.view.backgroundColor = UIColor.clear
+
+        setBackground(imageName: Constant.BackgroundName.basketball)
+
+    }
+
+    func getCourts() {
+
+        // MARK: Loading indicator
+        let activityData = ActivityData()
+        NVActivityIndicatorPresenter.sharedInstance.startAnimating(activityData)
+
+        print("===============")
+        print("Constant.CurrentCity.cityName: \(Constant.CurrentCity.cityName)")
+
+        BasketballCourtsProvider.shared.getApiData(city: Constant.CurrentCity.cityName, gymType: Constant.GymType.basketball) { (basketballCourts, error) in
+
+            if error == nil {
+
+                self.basketballCourts = basketballCourts!
+
+            } else {
+
+                // todo: error handling
+
+            }
+            print("basketballCourts?.count: \(String(describing: basketballCourts?.count))")
+            print("===============")
+            NVActivityIndicatorPresenter.sharedInstance.stopAnimating()
+        }
+    }
+
 }
